c4d3c217de8425b674fd376309ad19400d991899
Raghav <raghavelumalai@gmail.com>
2025-03-26 21:46:00 +0530

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+function toggleMenu(svg) {
+    const menu = document.getElementById("menu");
+    svg.classList.toggle("active"); // Toggle animation
+    menu.classList.toggle("show"); // Show/hide menu
+  }
+  
